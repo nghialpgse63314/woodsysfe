629ebfdd68c796c611ac9b1de2b01e0395b177e5
nghialpgse63314 <nghialpgse63314@fpt.edu.vn>
2024-07-10 22:25:47 +0700

    fetch data

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -1,24 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
 
-// import { initializeApp } from "firebase/app";
 
-import firebase from 'firebase/compat/app';
-import 'firebase/database';
-// import { getDatabase } from "firebase/database";
 const firebaseConfig = {
-  apiKey: "AIzaSyA_mI75RgxjPZz03Nq4jNLkLfil6QO6FYM",
-  authDomain: "fir-starter-587de.firebaseapp.com",
-  projectId: "fir-starter-587de",
-  databaseURL: "https://fir-starter-587de-default-rtdb.asia-southeast1.firebasedatabase.app/Product",
-  storageBucket: "fir-starter-587de.appspot.com",
-  messagingSenderId: "943898010882",
-  appId: "1:943898010882:web:9ed05d1e89ffacd68d0e1b",
-  measurementId: "G-3NTFYGJYBK"
+  apiKey: "AIzaSyASCFE_dSii2iTT2b2epagKGvh-lsdyQB8",
+  // authDomain: "testapi-98103.firebaseapp.com",
+  databaseURL: "https://wood--selling-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "wood--selling",
+//   storageBucket: "testapi-98103.appspot.com",
+//   messagingSenderId: "1030868661045",
+//   appId: "1:1030868661045:web:feea3da5bb26158178127c",
+//   measurementId: "G-SK1D0YDWCM"
 };
 
 // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const database = getDatabase(app);
-// export { database };
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-export default firebase;
+export default app;
